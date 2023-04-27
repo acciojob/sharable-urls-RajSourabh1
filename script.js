8cc4f4ad39bfa675e98ed4ec13a522e89198d676
Sourabh Kumar <createownpath111@gmail.com>
2023-04-27 16:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+function query(){
+    let url = document.getElementById("url");
+    let name1 = document.getElementById("name").value;
+    let year = document.getElementById("year").value;
+
+    console.log(name1,year);
+    if(name1 !="" && year !=""){
+        url.innerText = `https://localhost:8080/?name=${name1}&year=${year}`
+    }
+    else if(name1 !="" && year==""){
+        url.innerText = `https://localhost:8080/?name=${name1}`
+    }
+    else if(name1 =="" && year != ""){
+        url.innerText = `https://localhost:8080/?year=${year}`
+    }
+}
